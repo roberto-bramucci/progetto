462bfec144e6f14bb866aa43c8a1b7ed4cc4c3a2
Roberto Bramucci <s1087030@studenti.univpm.it>
2020-05-20 18:31:37 +0200

    secondo commit

--- progettoOOP/src/main/java/it/univpm/progettoOOP/ProgettoOopApplication.java
+++ progettoOOP/src/main/java/it/univpm/progettoOOP/ProgettoOopApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProgettoOopApplication {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO");
 		SpringApplication.run(ProgettoOopApplication.class, args);
 	}
-
 }
