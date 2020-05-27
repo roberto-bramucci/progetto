331b2cb7dff9e50b0b97670b6fda277c1d373cff
Roberto Bramucci <s1087030@studenti.univpm.it>
2020-05-27 12:47:55 +0200

    fix getMetadata

--- progettoOOP/src/main/java/it/univpm/progettoOOP/service/TweetService.java
+++ progettoOOP/src/main/java/it/univpm/progettoOOP/service/TweetService.java
@@ -2,11 +2,13 @@ package it.univpm.progettoOOP.service;
 
 import java.util.Collection;
 
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+
 import it.univpm.progettoOOP.model.Tweet;
 
 public interface TweetService {
 	
 	public Collection<Tweet> getData();
-	public String getMetadata();
+	public JsonSchema getMetadata();
 
 }
