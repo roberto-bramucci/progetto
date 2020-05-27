5dd82d813b1d2ecf9eeefbdae87e60e06d2e232c
stefanobonci <64318069+stefanobonci@users.noreply.github.com>
2020-05-27 17:33:44 +0200

    aggiornamento util

--- progettoOOP/src/main/java/it/univpm/progettoOOP/util/filter/FilterDistance.java
+++ progettoOOP/src/main/java/it/univpm/progettoOOP/util/filter/FilterDistance.java
@@ -1,26 +1,26 @@
 package it.univpm.progettoOOP.util.filter;
 
-import java.awt.geom.Point2D;
+
 
 public class FilterDistance {
-	private Point2D.Double point = new Point2D.Double(13.516584634780884, 43.616962794315526);
+	protected int distance;
 	
-	public FilterDistance(Point2D.Double point) {
-		this.point = point;
+	public FilterDistance(Integer distance) {
+		this.distance = distance;
 	}
 
 	/**
-	 * @return the point
+	 * @return the distance
 	 */
-	public Point2D.Double getPoint() {
-		return point;
+	public Integer getDistance() {
+		return distance;
 	}
 
 	/**
 	 * @param point the point to set
 	 */
-	public void setPoint(Point2D.Double point) {
-		this.point = point;
+	public void setDistance(Integer distance) {
+		this.distance = distance;
 	}
 	
 	
--- progettoOOP/src/main/java/it/univpm/progettoOOP/util/filter/FilterDistanceGreater.java
+++ progettoOOP/src/main/java/it/univpm/progettoOOP/util/filter/FilterDistanceGreater.java
@@ -1,5 +1,24 @@
 package it.univpm.progettoOOP.util.filter;
 
-public class FilterDistanceGreater {
 
+
+import it.univpm.progettoOOP.model.Tweet;
+
+public class FilterDistanceGreater extends FilterDistance implements Filter{
+
+	public FilterDistanceGreater(Integer distance) {
+		super(distance);
+		
+	}
+	
+	
+
+	@Override
+	public boolean filter(Tweet tweet) {
+		if(tweet.getDistance(tweet)>distance)
+			return true;
+		else
+			return false;
+	}
+	
 }
