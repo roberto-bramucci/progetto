1a6c88b65b79d035b48b8be0bbb90acec7d6a5af
stefanobonci <64318069+stefanobonci@users.noreply.github.com>
2020-05-28 16:12:53 +0200

    aggiornato filtro Filter2Distances

--- progettoOOP/src/main/java/it/univpm/progettoOOP/util/filter/Filter2Distances.java
+++ progettoOOP/src/main/java/it/univpm/progettoOOP/util/filter/Filter2Distances.java
@@ -7,6 +7,9 @@ public class Filter2Distances {
 	public Filter2Distances(Double distance1, Double distance2) {
 		this.distance1 = distance1;
 		this.distance2 = distance2;
+		if (distance1>distance2) {
+			throw new IllegalArgumentException("distance1 must be lower than distance2");
+		}
 	}
 
 	/**
