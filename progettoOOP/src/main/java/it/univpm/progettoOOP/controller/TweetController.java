8a14e9857226d3dfc5317d397fa3b14272cb2a52
Roberto Bramucci <s1087030@studenti.univpm.it>
2020-06-17 12:56:08 +0200

    risolto warning pom.xml

--- progettoOOP/src/main/java/it/univpm/progettoOOP/controller/TweetController.java
+++ progettoOOP/src/main/java/it/univpm/progettoOOP/controller/TweetController.java
@@ -7,7 +7,6 @@ import java.util.NoSuchElementException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
